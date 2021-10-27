5e6c73b9989fa3706a156c7546b027d870fa67ce
Janaina Neres <janaina.evangelistaneres@gmail.com>
2021-10-27 20:50:40 -0300

    Urls e views alterados

--- djangonal/app_fgv/views.py
+++ djangonal/app_fgv/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse('<h2>Funcionou</h2>')
+
+
+def mypage(request):
+    return render(request, 'app_fgv.html')
 # Create your views here.
