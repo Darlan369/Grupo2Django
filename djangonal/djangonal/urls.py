36dce012d977e6b386e00833d5921504722693a3
Marcelo Amaral <marceloamaral386@gmail.com>
2021-10-27 08:06:13 -0300

    Pequenas alterações

--- djangonal/djangonal/urls.py
+++ djangonal/djangonal/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'mapp/', include('mapp.urls'))
+    path(r'mapp/', include('mapp.urls')),
 ]
