8194b8c9fe0dc133ab4c84bc4fa98b0849c19b48
Janaina Neres <janaina.evangelistaneres@gmail.com>
2021-10-27 08:07:28 -0300

    virgulas

--- djangonal/djangonal/urls.py
+++ djangonal/djangonal/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'app_fgv/', include('app_fgv.urls'))
+    path(r'app_fgv/', include('app_fgv.urls')),
 ]
