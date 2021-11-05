a965f374843f8f1ec6a205a8839dd3a3cf24d59e
Marcelo Amaral <marceloamaral386@gmail.com>
2021-11-05 08:31:41 -0300

    Passo 5

--- djangonal/mapp/views.py
+++ djangonal/mapp/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.http.response import HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 def nada(request):
     return HttpResponse('<h2>Não tem nada aqui</h2>')
@@ -12,3 +14,26 @@ def quadro(request):
 
 def sono(request):
     return render(request, 'mapp/sono.html')
+
+def especial_int(request, param):
+    if param == 0:
+        return HttpResponse('<h2>Parâmetro de valor 0</h2> <h4>Não natural para alguns</h4>')
+    elif param % 2 == 0:
+        return HttpResponse('<h2>Parâmetro de valor par</h2>')
+    elif param % 2 == 1:
+        return HttpResponse('<h2>Parâmetro de valor ímpar</h2>')
+    else:
+        return HttpResponseNotFound('Página não existe.')
+
+def especial_str(request, param):
+    if param == 'vazio':
+        return HttpResponse('<h2>Você estuda cálculo<h2>')
+    elif param == 'dor':
+        return HttpResponse('<h2>Você estuda álgebra linear</h2>')
+    else:
+        return HttpResponseNotFound('Página não existe.')
+
+def redireciona(request):
+    # return HttpResponseRedirect('mapp/especial/vazio')
+    url_redirecionamento = reverse('especial_str', args = ['vazio'])
+    return HttpResponseRedirect(url_redirecionamento)
