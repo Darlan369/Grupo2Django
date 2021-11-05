6ed0dda77c252904570d20be15e2410262c28eca
Darlan369 <darlanaraujo369@gmail.com>
2021-11-05 15:57:47 -0300

    Parte 5

--- djangonal/Batata/urls.py
+++ djangonal/Batata/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.urls import path
 from Batata import views
 urlpatterns = [
-    path('gato1/', views.gato1, name='gato1'),
-    path('gato2/', views.gato2, name='gato2'),
-    path('index/', views.index, name='index')
+    path('gato1/<int:Tomate>', views.gato1, name='gato1'),
+    path('gato2/<str:Ovo>', views.gato2, name='gato2'),
+    path('index/', views.index, name='index'),
+    path('redireciona/', views.redireciona, name='redireciona')
 
 ]
+
+
+
--- djangonal/Batata/views.py
+++ djangonal/Batata/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse
+from django.http.response import HttpResponseNotFound, HttpResponseRedirect
 
 def index(request):
     return render(request, "Batata/index.html")
 
-def gato1(request):
-    return render(request, "Batata/gato1.html")
+def gato1(request, Tomate):
+    if Tomate == 1:
+        return render(request, "Batata/gato1.html")
+    else:
+        return render(request, "Batata/gato2.html")
 
-def gato2(request):
-    return render(request, "Batata/gato2.html")
+def gato2(request, Ovo):
+    Dicionario = {
+
+        'Criativo': Ovo
+
+    }
+    return render(request, "Batata/gato2.html", Dicionario)
+
+def redireciona(request):
+    r = reverse('gato1', args=['1'])
+    return HttpResponseRedirect(r)
