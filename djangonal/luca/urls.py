2279b5f2e91bd71cc2efc5cd022d558cd57012af
LucaEscopelli <luca.escopelli@gmail.com>
2021-11-05 08:30:53 -0300

    url dinamica

--- djangonal/luca/urls.py
+++ djangonal/luca/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from luca import views as luca_views
+from luca import views
 
 urlpatterns = [
-    path("", luca_views.luca, name="luca")
+    path("", views.index, name="index"),
+    path("luca/", views.luca, name="luca"),
+    path("special/<int:param>", views.special, name="special"),
+    path("redireciona/", views.redir, name="redireciona")
+
 ]
--- djangonal/luca/views.py
+++ djangonal/luca/views.py
@@ -1,6 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render
 
 # Create your views here.
 
-def luca(request):
+def index(request):
     return HttpResponse("<strong>Luca Lindo!!</strong>")
+
+def luca(request):
+    return render(request, 'luca/luca.html')
+
+def special(request, param):
+    if param%2 == 0:
+        return HttpResponse("Deu par!")
+
+    else:
+        return HttpResponse("Deu ímpar!")
+
+def redir(request):
+    direcionamento = reverse("luca")
+    return HttpResponseRedirect(direcionamento)
