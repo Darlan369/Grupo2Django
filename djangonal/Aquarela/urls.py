b09cfa03cc39c765aab75ca8b2e626d273386bdc
Amanda <nandaperez2002@gmail.com>
2021-11-05 08:59:19 -0300

    Redirecionamento, url dinâmica etc.

--- djangonal/Aquarela/urls.py
+++ djangonal/Aquarela/urls.py
@@ -4,4 +4,6 @@ from Aquarela import views
 urlpatterns = [
     path("", views.index, name ="index"),
     path("index/", views.index, name ="index"),
+    path("especial/<int:param>", views.especial, name="especial"),
+    path("redireciona/", views.redireciona, name="redireciona")
 ]
